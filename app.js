609cfcdc3d5e21dbfe99a784abd88dc1bd7256f7
Karsyz <mattkars@gmail.com>
2022-08-18 18:08:16 -0400

    added routes and helpers

--- app.js
+++ app.js
@@ -3,29 +3,59 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path:'./config/config.env' })
 
 // Passport Config
-
 require('./config/passport')(passport)
-
 
 connectDB()
 
 const app = express()
+
+//Body Parcer
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+
+// Method Override 
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+}))
 
 // Logging
 if (process.env.NODE_ENV === 'developement') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 // Handlebars (templating engine)
-app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs.engine({ 
+    helpers: {
+       formatDate,
+       stripTags,
+       truncate,
+       editIcon,
+       select,
+    }, 
+    defaultLayout: 'main', 
+    extname: '.hbs'
+    })
+)
+
 app.set('view engine', '.hbs')
 
 // Session Middleware
@@ -33,14 +63,22 @@ app.use(
     session({
         secret: 'keyboard cat',
         resave: false,
-        saveUninitialized: false
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     })
  )
-
 
 // Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req,res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -48,6 +86,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
 
 const PORT = process.env.PORT || 3000
 
@@ -55,3 +95,4 @@ app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+
